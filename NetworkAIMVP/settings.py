6a41b9e45dc7f013e6621873c45b8818d603ec12
abrham45 <85214695+abrham45@users.noreply.github.com>
2023-06-04 09:52:48 +0300

    Update settings.py

--- NetworkAIMVP/settings.py
+++ NetworkAIMVP/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'NetworkAIMVP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'NetworkAIMVP',
         'USER': 'postgres',
         'PASSWORD':'123',
         'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        
     }
 }
 
